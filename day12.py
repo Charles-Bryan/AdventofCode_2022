1dcd82a2fa3ff842aa208d63c24937419e461fe7
Charles-Bryan <charles.c.b2@outlook.com>
2022-12-11 21:53:09 -0500

    Added a template day file to save a little time each day

---
+++ day12.py
@@ -0,0 +1,18 @@
+from aocd import get_data
+
+# --- Day XX:
+# Getting the input
+raw_data = get_data(day=XX, year=2022)
+input_lines = raw_data.split('\n')
+
+
+
+# Part 0:
+
+
+# Part 1:
+
+
+# Part 2:
+
+print(0)
